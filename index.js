93d881aff0b209e958413d0861d55e2423a089e5
Pietro <pietro.passarelli@gmail.com>
2017-05-27 13:08:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var os = require('os')
 var path = require('path')
 
 var platform = os.platform()
-if (platform !== 'linux' && platform !== 'darwin' && platform !== 'win32') {
-  console.error('Unsupported platform.')
+//adding browser, for use case when module is bundled using browserify. and added to html using src.
+if (platform !== 'linux' && platform !== 'darwin' && platform !== 'win32' && platform !=="browser") {
+  console.error('Unsupported platform.', platform);
   process.exit(1)
 }
 
 var arch = os.arch()
-if (platform === 'darwin' && arch !== 'x32') {
+if (platform === 'darwin' && arch !== 'x64') {
   console.error('Unsupported architecture.')
   process.exit(1)
 }
